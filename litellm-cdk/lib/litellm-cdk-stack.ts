2b186bc3028c324c8c3243022b92fafdbe3c2edf
mirodrr2 <153310492+mirodrr2@users.noreply.github.com>
2025-01-13 12:36:55 -0500

    fix issue with waf blocking large request bodies (#21)
    Co-authored-by: michael rodriguez <mirodrr@amazon.com>

--- litellm-cdk/lib/litellm-cdk-stack.ts
+++ litellm-cdk/lib/litellm-cdk-stack.ts
@@ -485,6 +485,9 @@ export class LitellmCdkStack extends cdk.Stack {
               excludedRules: [
                 {
                   name: 'NoUserAgent_HEADER'
+                },
+                {
+                  name: 'SizeRestrictions_BODY'
                 }
               ]
             },
