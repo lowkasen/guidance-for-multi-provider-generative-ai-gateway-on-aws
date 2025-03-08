c73113735e76444c152e4eea78552aa13a8809e7
mirodrr2 <153310492+mirodrr2@users.noreply.github.com>
2025-03-07 22:00:37 -0500

    add service catalog registry application (#106)
    Co-authored-by: michael rodriguez <mirodrr@amazon.com>

--- litellm-terraform-stack/providers.tf
+++ litellm-terraform-stack/providers.tf
@@ -2,11 +2,18 @@ terraform {
   backend "s3" {}
 }
 
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
 locals {
+  SolutionNameKeySatisfyingRestrictions = "Guidance-for-Running-Generative-AI-Gateway-Proxy-on-AWS"
   common_labels = {
     project     = "llmgateway"
     AWSSolution = "ToDo"
     GithubRepo  = "https://github.com/aws-solutions-library-samples/"
+    SolutionID = "TBD"
+    SolutionNameKey = "Guidance for Running Generative AI Gateway Proxy on AWS"
+    SolutionVersionKey = "1.0.0"
   }
 }
 
@@ -14,6 +21,18 @@ locals {
 provider "aws" {
   default_tags {
     tags = local.common_labels
+  }
+}
+
+resource "aws_servicecatalogappregistry_application" "solution_application" {
+  name        = "${local.SolutionNameKeySatisfyingRestrictions}-${data.aws_region.current.name}-${data.aws_caller_identity.current.account_id}"
+  description = "Service Catalog application to track and manage all your resources for the solution ${local.common_labels.SolutionNameKey}"
+
+  tags = {
+    "Solutions:SolutionID"      = local.common_labels.SolutionID
+    "Solutions:SolutionName"    = local.common_labels.SolutionNameKey
+    "Solutions:SolutionVersion" = local.common_labels.SolutionVersionKey
+    "Solutions:ApplicationType" = "AWS-Solutions"
   }
 }
 
