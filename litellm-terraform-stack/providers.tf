1d6e9ae877765bd2a25536e19a102d64564dc686
Aamna <56640566+najmia@users.noreply.github.com>
2025-06-24 19:38:23 +0200

    Update providers.tf

--- litellm-terraform-stack/providers.tf
+++ litellm-terraform-stack/providers.tf
@@ -1,5 +1,11 @@
 terraform {
   backend "s3" {}
+  required_providers {
+      helm = {
+        source  = "hashicorp/helm"
+        version = "~> 2.0"
+      }
+    }
 }
 
 data "aws_caller_identity" "current" {}
